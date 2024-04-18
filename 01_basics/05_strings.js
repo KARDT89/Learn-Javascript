afc058cfa2244eb44b4cff9f8c52769e97204639
Tamal Sarkar <77930336+KARDT89@users.noreply.github.com>
2024-04-18 08:27:37 +0000

    string video

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,32 @@
+const name = "tamal"
+const repoCount = 50
+
+//console.log(name + repoCount + " Value");
+
+//console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Tamal-DT89-kar98')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase);
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('T'));
+
+const newString = gameName.substring(0,4)
+// console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+// console.log(anotherString);
+
+// const newString0ne = '    DT89    '
+// console.log(newString0ne);
+// console.log(newString0ne.trim());
+
+const url = "https://dt89.com/gaming%69kardt89"
+// console.log(url.replace('%69', '-')); 
+// console.log(url.includes('DT89'));
+
+console.log(gameName.split('-'));
